fe6729d35176bd25df7ed05d8cb3395e099e6d88
LaurenForster25 <lauren.forster2002@gmail.com>
2025-10-25 04:24:53 +0100

    Adjusting errors in code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,18 @@
 //code executes only when page is fully loaded 
 
 const buttons = document.getElementsByClassName("btn");
-const playerIcon = document.getElementById //add icon option to html code?
-const computerIcon = document.getElementById //add icon option to html code?
+const playerIcon = document.getElementById("player-icon");
+const computerIcon = document.getElementById("computer-icon");
 const messages = document.getElementById("result");
 const playerScore = document.getElementById("player-score");
 const computerScore = document.getElementById("computer-score");
 //const result = document.getElementById("result");
 const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
-for (let buttons of buttons) {
+for (let button of buttons) {
     buttons.addEventListener("click", function () {
         let playerChoice = this.getAttribute("data-choice");
-        playGame(playerChoice);
+        runGame(playerChoice);
     });
 }
 
@@ -21,9 +21,9 @@ for (let buttons of buttons) {
 
 function runGame(playerChoice) {
     playerIcon.src = `assets/images/${choices[playerChoice]}.png`
-    playerIcon.alt - choices[playerChoice];
+    playerIcon.alt = choices[playerChoice];
 
-    let computerChoice = Math.floor(Math.random() * 4); //mention this code snippet in credits
+    let computerChoice = Math.floor(Math.random() * choices.length); //mention this code snippet in credits
 
     computerIcon.src = `assets/images/${choices[computerChoice]}.png`;
     computerIcon.alt = choices[computerChoice];
@@ -47,7 +47,7 @@ const winsAgainst = {
 
 //DETERMINE WINNER 
 
-function findWinner(player, computer) {
+function chooseWinner(player, computer) {
     if (player === computer) {
         return `You've tied! ${player}.`;
     } else if (winsAgainst[player].includes(computer)){

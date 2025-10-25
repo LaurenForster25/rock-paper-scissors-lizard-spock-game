84c50100a6cbbb881b42445b95dd4bbb3388a2f0
LaurenForster25 <lauren.forster2002@gmail.com>
2025-10-25 03:30:28 +0100

    if else code to determine who wins

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,6 +52,12 @@ const winsAgainst = {
 
 };
 
+if (winsAgainst[player].includes(computer)){
+    return `You win! (${player} beats ${computer})`;
+} else {
+    return `You lose! (${computer} beats ${player})`;
+}
+
 
 //code to determine winner 
 

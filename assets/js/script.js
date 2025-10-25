b43b5caba87bbae5f8b956b1aa868fbc916d81b3
LaurenForster25 <lauren.forster2002@gmail.com>
2025-10-25 02:41:58 +0100

    Including constants for DOM elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 //event listeners - when first icon is clciked 
 //code executes only when page is fully loaded 
 
+const buttons = document.getElementsByClassName("btn");
+const playerIcon = document.getElementById
+const computerIcon = document.getElementById
+const messages = document.getElementById
+const playerScore = document.getElementById
+const computerScore = document.getElementById
+
 
 function runGame() {
 
